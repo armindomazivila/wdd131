9961ab234e4b1749f0b1f187acb1cc6dbd64ab4c
github_username_armindomazivila <armindo.mazivila1990@gmail.com>
2026-09-12 16:41:18 +0200

    Update the last changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -13,9 +13,11 @@ menuButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
 
     if (navigation.classList.contains("open")) {
-        menuButton.textContent = "✖";
+        menuButton.textContent = "✕";
+        menuButton.setAttribute("aria-label", "Close navigation menu");
     } else {
         menuButton.textContent = "☰";
+        menuButton.setAttribute("aria-label", "Open navigation menu");
     }
 
 });
