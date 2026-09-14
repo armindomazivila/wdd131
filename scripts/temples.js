82291c2bec8c179048436706fb2c227c097de0e8
github_username_armindomazivila <armindo.mazivila1990@gmail.com>
2026-09-14 16:56:31 +0200

    update the changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,23 +1,15 @@
-const year = document.querySelector("#CurrentYear");
-const lastModified = document.querySelector("#lastModified");
-
-year.textContent = new Date().getFullYear();
-
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
-
 const menuButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 
 menuButton.addEventListener("click", () => {
-
     navigation.classList.toggle("open");
 
-    if (navigation.classList.contains("open")) {
-        menuButton.textContent = "✕";
-        menuButton.setAttribute("aria-label", "Close navigation menu");
-    } else {
-        menuButton.textContent = "☰";
-        menuButton.setAttribute("aria-label", "Open navigation menu");
-    }
+    const isOpen = navigation.classList.contains("open");
 
+    menuButton.textContent = isOpen ? "✕" : "☰";
+    menuButton.setAttribute(
+        "aria-label",
+        isOpen ? "Close navigation menu" : "Open navigation menu"
+    );
+    menuButton.setAttribute("aria-expanded", isOpen);
 });
