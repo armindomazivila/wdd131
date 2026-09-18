d7c2e5785a5d8d65ef8c86e2759015e5304e4229
github_username_armindomazivila <armindo.mazivila1990@gmail.com>
2026-09-18 18:58:24 +0200

    Add the changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,21 @@
-document.getElementById("year").textContent =
+const temperature = 25;
+const windSpeed = 10;
+
+function calculateWindChill(temperature, windSpeed) {
+    return 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16));
+}
+
+const windChill = document.querySelector("#wind-chill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent =
+        `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
+
+document.querySelector("#currentyear").textContent =
     new Date().getFullYear();
 
-document.getElementById("lastModified").textContent =
+document.querySelector("#lastModified").textContent =
     document.lastModified;
