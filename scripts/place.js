6a9cf90b32c257054767cbe4c99666bfe4ca84a4
github_username_armindomazivila <armindo.mazivila1990@gmail.com>
2026-09-18 19:27:31 +0200

    Update de changes

--- scripts/place.js
+++ scripts/place.js
@@ -5,13 +5,13 @@ function calculateWindChill(temperature, windSpeed) {
     return 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16));
 }
 
-const windChill = document.querySelector("#wind-chill");
+const windChillElement = document.querySelector("#wind-chill");
 
 if (temperature <= 10 && windSpeed > 4.8) {
-    windChill.textContent =
+    windChillElement.textContent =
         `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
 } else {
-    windChill.textContent = "N/A";
+    windChillElement.textContent = "N/A";
 }
 
 document.querySelector("#currentyear").textContent =
